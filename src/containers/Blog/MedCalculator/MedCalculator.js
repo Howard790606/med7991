ddcf9c67a7a96e1299f586c7e10efa0f8db17586
howard790606 <r07945018@ntu.edu.tw>
2019-05-16 14:18:00 +0800

    first commit

---
+++ src/containers/Blog/MedCalculator/MedCalculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import CCr from "./CCr";
+
+export default class MedCalculator extends Component {
+
+    render() {
+        return (
+            <div>
+                <CCr/><br></br>
+            </div>
+        );
+    }
+}
