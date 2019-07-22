74fe7fc7da10ef3e14aa2db30d948e0071488bbd
howard790606 <r07945018@ntu.edu.tw>
2019-07-23 00:23:29 +0800

    web

--- src/containers/Blog/MedCalculator/MedCalculator.js
+++ src/containers/Blog/MedCalculator/MedCalculator.js
@@ -9,6 +9,7 @@ export default class MedCalculator extends Component {
             <div>
                 <CCr/><br></br>
                 <ABG_Interpretation/>
+                <p>Reference: The ICU book, 4th edition, Paul L. Marino</p>
             </div>
         );
     }
