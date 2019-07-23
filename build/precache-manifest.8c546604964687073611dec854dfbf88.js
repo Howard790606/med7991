98352bb74d8c19c4688a0bba033d473e27f85cdf
howard790606 <r07945018@ntu.edu.tw>
2019-07-23 11:59:17 +0800

    web

--- build/precache-manifest.8c546604964687073611dec854dfbf88.js
+++ build/precache-manifest.8c546604964687073611dec854dfbf88.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f97a8d79825e355ea5cadf62623fae3c",
+    "revision": "182c80532824d7f14d68575d62e6ca1b",
     "url": "/index.html"
   },
   {
-    "revision": "38ec88e05835873da164",
+    "revision": "11a757fee8ba14adc533",
     "url": "/static/css/main.ca343bee.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f4b7c823.chunk.js"
   },
   {
-    "revision": "38ec88e05835873da164",
-    "url": "/static/js/main.0dc7f51c.chunk.js"
+    "revision": "11a757fee8ba14adc533",
+    "url": "/static/js/main.e979b8ed.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- src/containers/Blog/MedCalculator/MedCalculator.js
+++ src/containers/Blog/MedCalculator/MedCalculator.js
@@ -7,7 +7,7 @@ export default class MedCalculator extends Component {
     render() {
         return (
             <div>
-                <CCr/><br></br>
+                <CCr/>
                 <p>Reference:  1. Cockcroft-Gault Equation 2. MDRD quation</p>
                 <ABG_Interpretation/>
                 <p>Reference: The ICU book, 4th edition, Paul L. Marino</p>
