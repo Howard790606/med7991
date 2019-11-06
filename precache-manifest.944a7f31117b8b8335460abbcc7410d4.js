dd65a9d5a8543dc734ab8abd0a10152a3784a87d
howard790606 <r07945018@ntu.edu.tw>
2019-11-06 21:55:47 +0800

    Updates

--- precache-manifest.944a7f31117b8b8335460abbcc7410d4.js
+++ precache-manifest.944a7f31117b8b8335460abbcc7410d4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "112b159eb09bdfb46b150c4a00029f27",
+    "revision": "eb48c0932c0bf74b229d78a62909d6f9",
     "url": "/index.html"
   },
   {
-    "revision": "3c5fc11d4ede7f8e8e29",
+    "revision": "7fbd9302d8a77d2507ed",
     "url": "/static/css/main.ca343bee.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.570e1fcb.chunk.js"
   },
   {
-    "revision": "3c5fc11d4ede7f8e8e29",
-    "url": "/static/js/main.e70fda65.chunk.js"
+    "revision": "7fbd9302d8a77d2507ed",
+    "url": "/static/js/main.2708031c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
