de30b481e8d4bc314653d2e3fe6973185b4f146d
howard790606 <r07945018@ntu.edu.tw>
2020-05-20 17:02:35 +0800

    latest

---
+++ build/precache-manifest.f8f2fdc0d55e62d36996b6d5dad58613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fefda3d2d5d09b3319d2ba48d48eba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ae3159880b7f6a6e0d6",
+    "url": "/static/css/main.ca343bee.chunk.css"
+  },
+  {
+    "revision": "badfe2759b4a90d203ef",
+    "url": "/static/js/2.775b74c4.chunk.js"
+  },
+  {
+    "revision": "bc23c04eec3c55de9f1e7db1db146add",
+    "url": "/static/js/2.775b74c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae3159880b7f6a6e0d6",
+    "url": "/static/js/main.c0b60629.chunk.js"
+  },
+  {
+    "revision": "fd6838b2d5819b4d29a4",
+    "url": "/static/js/runtime-main.0178ec67.js"
+  }
+]);
