e294cf999933e30f9abd754adfd2b1dc6628ce1e
howard790606 <r07945018@ntu.edu.tw>
2022-01-26 14:26:27 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0638f7e81ea9b9fd991be0e3ef34cf50.js"
+  "/precache-manifest.76debe2cd6b200b455938578fc948404.js"
 );
 
 self.addEventListener('message', (event) => {
